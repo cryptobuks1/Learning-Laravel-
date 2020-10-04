7d5cb62b8a3d0ab16095fd91472cdd816c3cf3ca
Quang Anh <65335643+nqa0110@users.noreply.github.com>
2020-10-04 11:30:17 +0700

    1

--- PHP/hs.php
+++ PHP/hs.php
@@ -1,8 +1,26 @@
 <?php
 
-function tinhtong($n)
-{
-    if ($n == 1){ return $n; }
-    return ($n * tinhtong($n-1));
+$json = '[
+    {
+        "company_id":1
+    },
+    {
+        "company_id":2
+    }
+]';
+$mang = json_decode($json);
+
+$dem = count($mang);
+$fl = false;
+for ($i=0; $i < ($dem); $i++) { 
+    if(($mang[$i]->company_id) == 3){
+        $fl == true;
+    }
 }
-echo tinhtong(5);
+
+if ($fl) {
+    echo 'k dc thuc hien';
+}
+else {
+    echo 'dc thuc hien';
+}
