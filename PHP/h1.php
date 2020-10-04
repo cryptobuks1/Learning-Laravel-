fed49c6767f31472a6b7a15746186265b3bf32b9
Quang Anh <65335643+nqa0110@users.noreply.github.com>
2020-10-04 13:07:03 +0700

    1
    1

--- PHP/h1.php
+++ PHP/h1.php
@@ -15,13 +15,11 @@ $json3 = '[
         "company_id":3
     }
 ]';
-
 $arr = [json_decode($json1),json_decode($json2),json_decode($json3)];
-// print_r($arr[2][0]->company_id);
 $dem = count($arr);
 $fl = false;
 for ($i=0; $i < $dem; $i++) { 
-    if (($arr[$i][0]->company_id) == 0) {
+    if (($arr[$i][0]->company_id) == 1) {
         $fl = true;
     }
 }
