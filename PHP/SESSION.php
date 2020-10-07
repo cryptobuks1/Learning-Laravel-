a65422c2e8653b8efb7154a9c57a867c4377eefb
nqanh@vitenet.net <username@gmail.com>
2020-10-07 17:30:36 +0700

    1
    1

---
+++ PHP/SESSION.php
@@ -0,0 +1,14 @@
+<!-- phía trên đầu mỗi file để sử dụng session -->
+<!-- Nếu bạn dùng nhiều file include lẫn nhau thì đặt nó ở file chính -->
+<?php session_start(); ?>
+  
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <div>TODO write content</div>
+    </body>
+</html>
