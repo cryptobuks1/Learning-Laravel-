ac43458f9a6eb6898a930f285fba8c635ad25fa7
Quang Anh <65335643+nqa0110@users.noreply.github.com>
2020-10-08 22:41:22 +0700

    1
    1

--- PHP/COOKIE.php
+++ PHP/COOKIE.php
@@ -1,33 +1,40 @@
 <!DOCTYPE html>
   
 <?php
-    //set name, value, thời gian sống của cookie
-    setcookie('username', 'thehalfheart', time() + 3600);
-
     //khi ấn vào submit có name delete-cookie
     if (isset($_POST['delete-cookie'])) {
         // Xóa cookie name
         // để xóa cookie => thiết lập thời gian âm >= thời gian sống của cookie
         setcookie("username", "", time()-3600);
     }
+    //khi ấn vào submit có name delete-cookie
+    if (isset($_POST['save-cookie'])) {
+        // SAVE cookie name
+        // set name, value, thời gian sống của cookie
+        setcookie('username', 'thehalfheart', time() + 3600);
+    }
 ?>
-  
+
 <html>
     <head>
         <title></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        <h1>
-            <?php
-                if (isset($_COOKIE['username']))
-                {
-                echo $_COOKIE['username'];
-                }
-            ?>
-        </h1>
         <form method="POST" action="">
+            <h1>
+                <?php
+                    if (isset($_COOKIE['username']))
+                    {
+                        echo 'COOKIE là '.$_COOKIE['username'];
+                    }
+                    else{
+                        echo 'Đã Xóa COOKIE';
+                    }
+                ?>
+            </h1>
             <input type="submit" name="delete-cookie" value="Xóa COOKIE"/>
+            <input type="submit" name="save-cookie" value="SAVE COOKIE"/>
         </form>
     </body>
 </html>
